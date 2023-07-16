da1ed071f1b5875e0c43233ba998197929c1f2cc
Amr <amr.abdoullah@gmail.com>
2023-07-16 09:53:39 +0300

    Projects: Handle project delete on cascate

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,11 @@
 class Project < ApplicationRecord
   belongs_to :user
   has_many :tasks
+
+  before_destroy :remove_project_id_from_tasks
+
+  private
+  def remove_project_id_from_tasks
+    tasks.update_all(project_id: nil)
+  end
 end
